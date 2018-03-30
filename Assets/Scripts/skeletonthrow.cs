343b809798f58a215736b6c20b9102dad4ded18c
nitrogendragon <dirtboy4fun@gmail.com>
2018-03-30 14:58:36 -0500

    added a scary skeleton
    skeleton with a hella bad animation for throwing the baseball

---
+++ Assets/Scripts/skeletonthrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeletonthrow : MonoBehaviour {
+
+
+        public Animation anim;
+        void Start()
+        {
+            anim = GetComponent<Animation>();
+            foreach (AnimationState state in anim)
+            {
+                state.speed = 2F;
+            }
+        }
+    private void Update()
+    {
+        anim.Play();
+    }
+}
