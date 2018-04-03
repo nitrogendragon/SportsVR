3dc6a3bd3bb84618736c6c0909a4f6b110d84652
nitrogendragon <dirtboy4fun@gmail.com>
2018-04-02 19:08:42 -0500

    changed up basketball abit

---
+++ Assets/BasketballScenestuff/basketball sounds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basketballsounds : MonoBehaviour {
+    public GameObject ground;
+    public GameObject basketballmodel;
+    public GameObject scoreupdater;
+    public GameObject backboard;
+    public AudioSource hitground;
+    public AudioSource scored;
+    public AudioSource hitsurface;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject == ground || basketballmodel)
+        {
+            hitground.Play();
+        }
+        else if(other.gameObject == scoreupdater)
+        {
+            scored.Play();
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
